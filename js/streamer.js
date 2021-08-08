95630b13ff142ebb42757626314df092fdb13dcf
Chris Ootes <chris_ootes0101@hotmail.com>
2021-08-08 14:11:40 +0200

    add more comments

--- js/streamer.js
+++ js/streamer.js
@@ -7,61 +7,71 @@ function update() {
     $('.row').masonry({percentPosition: true});
 }
 
-// add next page to grid
+// Add next page to grid
 function append() {
     console.debug('Last: ', last_page);
+    // The link to next pages is in a href with class next-page
     next_page_link = $(".next-page").attr('href');
+    // Check if next page is already is added to prevent duplicates
     if (last_page == next_page_link) {
         console.debug('Already added');
     } else {
         last_page = next_page_link;
         console.debug('Adding: ', next_page_link);
-        //$(".row").append("Some appended text.");
+        // Request next page
+        // With ?infinite=true we dont have to find de items on the page DOM
+        // It gives only the html code with items
         $.get(next_page_link + '?infinite=true', function (data, status) {
-            // make jQuery object
+            // Make jQuery object
             $last_elems = $(data);
-            // add jQuery object
+            // Add jQuery object and update masonry
             $('.row').append($last_elems).masonry('appended', $last_elems);
-            // wait images to be loaded
+            // Wait images to be loaded and update masonry again
             setTimeout(update, 2000);
-            // Update next page url replace last 5 letters with new id
-            // maybe use split('/')
+            // Replace next page url with new one
+            // The last 5 characters of the path are the id of last post
+            // TODO Maybe lass hacky to use split('/')
             new_page = last_page.slice(0, -6) + $last_elems.get(-1).id; 
             $(".next-page").attr('href', new_page)
-            // enable scroll events
+            // Enable scroll events
             $(window).on("scroll", scroll_event);
         });
     };
 }
 
-// wait for scroll
+// Handle scroll event
 function scroll_event() {
     let scroll_top = $(window).scrollTop();
-    console.debug('scroll_top: ', scroll_top);
+    console.debug('Scroll position: ', scroll_top);
     let scroll_height = document.body.parentElement.scrollHeight;
     let client_height = document.body.parentElement.clientHeight;
     let pixels_togo = scroll_height - (scroll_top + client_height);
     if (pixels_togo < 2000) {
-        // disable scroll events
+        // Disable scroll events so we don't get million updates
         $(window).off("scroll", scroll_event);
+        // Append new posts
         append();
     };
 }
 
-// wait for images to be loaded
+// Wait for images to be loaded
 $(window).on("load", function () {
+    // Update masonry
     update();
     setTimeout(update, 2000);
 
-    // add images if there is no scroll bar
+    // Add images if there is no scroll bar
+    // If there is no scroll bar than there are no scroll events 
     let scroll_height = document.body.parentElement.scrollHeight;
     let client_height = document.body.parentElement.clientHeight;
     while (scroll_height == client_height) {
         scroll_height = document.body.parentElement.scrollHeight;
         client_height = document.body.parentElement.clientHeight;
+        // Append new posts
         append();
+        // TODO remove
         break;
     };
-    // enable scroll events
+    // Enable scroll events
     $(window).on("scroll", scroll_event);
 });
