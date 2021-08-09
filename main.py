c1b717a2c4aa4314286e10fb74a4bc1f177b7d9d
Chris Ootes <c.l.m.ootes@isala.nl>
2021-08-09 11:08:59 +0200

    renamed

---
+++ main.py
@@ -0,0 +1,166 @@
+#!python
+
+import time
+import datetime
+from urllib import parse
+
+import requests
+
+import config
+import server
+import reddit
+
+def format_post(post) -> str:
+    """
+    This function generates a embedded card html code from a PRAW post object
+    """
+    good_content = reddit.check(post)
+    if good_content:
+        parsed = parse.urlparse(post.url)
+        response = requests.head(post.url)
+        print(f"Sending {post.id}")
+        content_type = response.headers.get('content-type', '')
+        print(f"Content type {content_type}")
+        if 'image' in content_type:
+            return f"""
+                <div id="{post.id}" class="col-sm-12 col-lg-6 col-xxl-4">
+                    <div class="card">
+                        <img class="card-img-top" src="{post.url}">
+                        <div class="card-body">
+                            <h5 class="card-title">{post.title} <a href="http://old.reddit.com/comments/{post.id}" class="btn btn-primary">Comments</a></h5>
+                        </div>
+                    </div>
+                </div>
+            """
+        elif 'imgur' in post.url:
+            imgur_id = parsed.path.split('.')[0].split('/')[-1]
+            return f"""
+                <div id="{post.id}" class="col-sm-12 col-lg-6 col-xxl-4">
+                    <div class="card">
+                        <iframe allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" class="imgur-embed-iframe-pub" scrolling="no" src="http://imgur.com/{imgur_id}/embed" id="imgur-embed-iframe-pub-{imgur_id}"></iframe>
+                        <div class="card-body">
+                            <h5 class="card-title">{post.title} <a href="http://old.reddit.com/comments/{post.id}" class="btn btn-primary">Comments</a></h5>
+                        </div>
+                    </div>
+                </div>
+            """
+        elif 'redgif' in post.url:
+            redgifs_id = parsed.path.split('.')[0].split('/')[-1]
+            return f"""
+                <div id="{post.id}" class="col-sm-12 col-lg-6 col-xxl-4">
+                    <div class="card">
+                        <div style='position:relative; padding-bottom:88.67%;'>
+                            <iframe src='https://redgifs.com/ifr/{redgifs_id}' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0;' allowfullscreen></iframe>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">{post.title} <a href="http://old.reddit.com/comments/{post.id}" class="btn btn-primary">Comments</a></h5>
+                        </div>
+                    </div>
+                </div>
+            """
+        elif content_type == '':
+            return f"""
+                <div id="{post.id}" class="col-sm-12 col-lg-6 col-xxl-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-grid gap-2">
+                                <a href="{post.url}" class="btn btn-secondary">Video</a>
+                            </div>
+                            <h5 class="card-title">{post.title} <a href="http://old.reddit.com/comments/{post.id}" class="btn btn-primary">Comments</a></h5>
+                        </div>
+                    </div>
+                </div>
+            """
+        elif 'text/html' in content_type:
+            return f"""
+                <div id="{post.id}" class="col-sm-12 col-lg-6 col-xxl-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-grid gap-2">
+                                <a href="{post.url}" class="btn btn-primary">Link</a>
+                            </div>
+                            <h5 class="card-title">{post.title} <a href="http://old.reddit.com/comments/{post.id}" class="btn btn-primary">Comments</a></h5>
+                        </div>
+                    </div>
+                </div>
+            """
+        else:
+            print("TODO")
+    return ''
+
+
+def streamer_callback(path: str):
+    """
+    Callback generator for the streamer server that yields utf-8 bytes of html code
+    """
+
+    # parse path
+    path_parsed = parse.urlparse(path)
+    #print(f"Path: {path_parsed}")
+    splitted = path_parsed.path.split('/')
+    subreddit = 'all'
+    if len(splitted) > 1:
+        if splitted[1] != '':
+            subreddit = splitted[1]
+    page = ''
+    if len(splitted) > 2:
+        page = splitted[2]
+
+    # parse query
+    query = parse.parse_qs(path_parsed.query)
+    infinite = query.get('infinite', False)
+    
+    if infinite:
+        print(f"Infinite {subreddit} on page {page}")
+        posts = reddit.generator(subreddit=subreddit, last_id=page, amount=config.PAGE_ITEM_AMOUNT)
+        for post in posts:
+            last_id = post.id
+            yield format_post(post).encode('utf-8')
+
+    else:
+        template = open('template_page.html', 'r')
+        
+        # Loop template
+        while True:
+            line = template.readline()
+            # Chek for end file
+            if line == '':
+                break
+            # Check for template item
+            elif '$' in line:
+                splitted = line.split('$')
+                #print(f"Splitted template: {splitted}")
+                # Send everythin before first $
+                yield splitted[0].encode('utf-8')
+                # Send between
+        
+                # Template key: date
+                if splitted[1] == 'date':
+                    yield datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S").encode('utf-8')
+
+                # Template key: page
+                elif splitted[1] == 'page':
+                    yield f"/{subreddit}/{last_id}".encode('utf-8')
+
+                # Template key: items
+                elif splitted[1] == 'items':
+                    print(f"Getting {subreddit} on page {page}")
+                    posts = reddit.generator(subreddit=subreddit, last_id=page, amount=config.PAGE_ITEM_AMOUNT)
+                    for post in posts:
+                        last_id = post.id
+                        yield format_post(post).encode('utf-8')
+
+                # Send everythin after second $
+                yield splitted[2].encode('utf-8')
+
+            # Everything else is static content
+            else:
+                yield line.encode('utf-8')
+
+
+if __name__ == "__main__":
+    streamer.create_server(
+        config.SERVER_HOSTNAME,
+        config.SERVER_PORT,
+        config.STATIC_FILES,
+        streamer_callback)
--- server.py
+++ server.py
@@ -1,166 +1,55 @@
-#!python
+"""
+Stream HTML from generator
+For example the reddit api is rate limited
+With this you can send data between api calls instead of just waiting
+"""
 
-import time
-import datetime
-from urllib import parse
+import sys
+import io
+import http.server
 
-import requests
 
-import config
-import streamer
-import reddit
+class RequestHandler(http.server.SimpleHTTPRequestHandler):
+    protocol_version = "HTTP/1.0"
+    def callback(a): return a
+    static_files = []
 
-def format_post(post) -> str:
-    """
-    This function generates a embedded card html code from a PRAW post object
-    """
-    good_content = reddit.check(post)
-    if good_content:
-        parsed = parse.urlparse(post.url)
-        response = requests.head(post.url)
-        print(f"Sending {post.id}")
-        content_type = response.headers.get('content-type', '')
-        print(f"Content type {content_type}")
-        if 'image' in content_type:
-            return f"""
-                <div id="{post.id}" class="col-sm-12 col-lg-6 col-xxl-4">
-                    <div class="card">
-                        <img class="card-img-top" src="{post.url}">
-                        <div class="card-body">
-                            <h5 class="card-title">{post.title} <a href="http://old.reddit.com/comments/{post.id}" class="btn btn-primary">Comments</a></h5>
-                        </div>
-                    </div>
-                </div>
-            """
-        elif 'imgur' in post.url:
-            imgur_id = parsed.path.split('.')[0].split('/')[-1]
-            return f"""
-                <div id="{post.id}" class="col-sm-12 col-lg-6 col-xxl-4">
-                    <div class="card">
-                        <iframe allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" class="imgur-embed-iframe-pub" scrolling="no" src="http://imgur.com/{imgur_id}/embed" id="imgur-embed-iframe-pub-{imgur_id}"></iframe>
-                        <div class="card-body">
-                            <h5 class="card-title">{post.title} <a href="http://old.reddit.com/comments/{post.id}" class="btn btn-primary">Comments</a></h5>
-                        </div>
-                    </div>
-                </div>
-            """
-        elif 'redgif' in post.url:
-            redgifs_id = parsed.path.split('.')[0].split('/')[-1]
-            return f"""
-                <div id="{post.id}" class="col-sm-12 col-lg-6 col-xxl-4">
-                    <div class="card">
-                        <div style='position:relative; padding-bottom:88.67%;'>
-                            <iframe src='https://redgifs.com/ifr/{redgifs_id}' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0;' allowfullscreen></iframe>
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">{post.title} <a href="http://old.reddit.com/comments/{post.id}" class="btn btn-primary">Comments</a></h5>
-                        </div>
-                    </div>
-                </div>
-            """
-        elif content_type == '':
-            return f"""
-                <div id="{post.id}" class="col-sm-12 col-lg-6 col-xxl-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="d-grid gap-2">
-                                <a href="{post.url}" class="btn btn-secondary">Video</a>
-                            </div>
-                            <h5 class="card-title">{post.title} <a href="http://old.reddit.com/comments/{post.id}" class="btn btn-primary">Comments</a></h5>
-                        </div>
-                    </div>
-                </div>
-            """
-        elif 'text/html' in content_type:
-            return f"""
-                <div id="{post.id}" class="col-sm-12 col-lg-6 col-xxl-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="d-grid gap-2">
-                                <a href="{post.url}" class="btn btn-primary">Link</a>
-                            </div>
-                            <h5 class="card-title">{post.title} <a href="http://old.reddit.com/comments/{post.id}" class="btn btn-primary">Comments</a></h5>
-                        </div>
-                    </div>
-                </div>
-            """
+    def do_GET(self):
+        """
+        Overloaded but for static files use original function with super().do_GET()
+        """
+        # Static files
+        if self.path in self.static_files:
+            print(f"Static: {self.path}")
+            super().do_GET()
+        # Dynamic content based on path
         else:
-            print("TODO")
-    return ''
+            print(f"Dynamic: {self.path}")
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            # Put generator output in http file
+            for i in self.callback(self.path):
+                self.wfile.write(i)
 
 
-def streamer_callback(path: str):
+def create_server(hostname, serverport, static_files, callback):
     """
-    Callback generator for the streamer server that yields utf-8 bytes of html code
+    Create streaming server
     """
+    print("Creating server")
 
-    # parse path
-    path_parsed = parse.urlparse(path)
-    #print(f"Path: {path_parsed}")
-    splitted = path_parsed.path.split('/')
-    subreddit = 'all'
-    if len(splitted) > 1:
-        if splitted[1] != '':
-            subreddit = splitted[1]
-    page = ''
-    if len(splitted) > 2:
-        page = splitted[2]
+    # Setup handler
+    handler = RequestHandler
+    handler.callback = callback
+    handler.static_files = static_files
 
-    # parse query
-    query = parse.parse_qs(path_parsed.query)
-    infinite = query.get('infinite', False)
-    
-    if infinite:
-        print(f"Infinite {subreddit} on page {page}")
-        posts = reddit.generator(subreddit=subreddit, last_id=page, amount=config.PAGE_ITEM_AMOUNT)
-        for post in posts:
-            last_id = post.id
-            yield format_post(post).encode('utf-8')
-
-    else:
-        template = open('template_page.html', 'r')
-        
-        # Loop template
-        while True:
-            line = template.readline()
-            # Chek for end file
-            if line == '':
-                break
-            # Check for template item
-            elif '$' in line:
-                splitted = line.split('$')
-                #print(f"Splitted template: {splitted}")
-                # Send everythin before first $
-                yield splitted[0].encode('utf-8')
-                # Send between
-        
-                # Template key: date
-                if splitted[1] == 'date':
-                    yield datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S").encode('utf-8')
-
-                # Template key: page
-                elif splitted[1] == 'page':
-                    yield f"/{subreddit}/{last_id}".encode('utf-8')
-
-                # Template key: items
-                elif splitted[1] == 'items':
-                    print(f"Getting {subreddit} on page {page}")
-                    posts = reddit.generator(subreddit=subreddit, last_id=page, amount=config.PAGE_ITEM_AMOUNT)
-                    for post in posts:
-                        last_id = post.id
-                        yield format_post(post).encode('utf-8')
-
-                # Send everythin after second $
-                yield splitted[2].encode('utf-8')
-
-            # Everything else is static content
-            else:
-                yield line.encode('utf-8')
-
-
-if __name__ == "__main__":
-    streamer.create_server(
-        config.SERVER_HOSTNAME,
-        config.SERVER_PORT,
-        config.STATIC_FILES,
-        streamer_callback)
+    # Setup server
+    server = http.server.HTTPServer((hostname, serverport), handler)
+    print(f"Starting server on http://{hostname}:{serverport}")
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    server.server_close()
+    print("Server stopped")
